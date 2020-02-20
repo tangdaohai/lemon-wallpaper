629b1fddbaaa04c2498e57e903198ca903d1c62e
唐道海 <tangdaohai@outlook.com>
2020-02-20 21:02:20 +0800

    feat(ImageList): 完善图片展示布局

--- src/ImageList.tsx
+++ src/ImageList.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-// import Image from './images/test.jpg'
-
+import { Grid, Card, CardMedia, CardActionArea, CardActions, IconButton, Button } from '@material-ui/core'
+import { ArrowDownward } from '@material-ui/icons'
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1
     },
-    img: {
+    card: {
       width: '100%',
-      height: '300px',
-      objectFit: 'contain'
+      borderRadius: 0
+    },
+    cardMedia: {
+      height: 0,
+      paddingTop: '56.25%',
+      backgroundSize: 'contain'
+    },
+    cardActions: {
+      // height: '60px',
+      // justifyContent: 'center'
     }
   })
 )
@@ -20,8 +27,18 @@ export default function ImageList () {
   const classes = useStyles()
 
   const imgGrids = new Array(15).fill(0).map((val, index) => (
-    <Grid key={index} item xl={2} lg={4} md={6} sm={12}>
-      <img className={classes.img} src='/images/test.jpg' alt='图片测试' />
+    <Grid key={index} item xl={3} lg={4} md={6} sm={12}>
+      <Card className={classes.card} raised>
+        <CardActionArea>
+          <CardMedia className={classes.cardMedia} image={`/images/test${index % 2}.jpg`} title='Lemon wallpaper' />
+        </CardActionArea>
+        <CardActions className={classes.cardActions}>
+          <IconButton aria-label='add to favorites'>
+            <ArrowDownward />
+          </IconButton>
+          <Button variant='contained' color='primary'>设置桌面</Button>
+        </CardActions>
+      </Card>
     </Grid>
   ))
 
