d3dd8802a052d212059de1b94558e78c6948c924
唐道海 <tangdaohai@outlook.com>
2020-02-21 21:52:32 +0800

    feat(app.tsx): 补充右侧交互组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import clsx from 'clsx'
-import { ThemeProvider, makeStyles, Theme, createStyles, useTheme } from '@material-ui/core/styles'
+import { ThemeProvider, makeStyles, Theme, createStyles, useTheme, fade } from '@material-ui/core/styles'
 import createCustomTheme from './theme'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Drawer from '@material-ui/core/Drawer'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Tooltip from '@material-ui/core/Tooltip'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import InputBase from '@material-ui/core/InputBase'
 import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import GitHub from '@material-ui/icons/GitHub'
+import Brightness7Icon from '@material-ui/icons/Brightness7'
+import Brightness4Icon from '@material-ui/icons/Brightness4'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import ListItem from '@material-ui/core/ListItem'
 import { ListItemText, ListItemIcon } from '@material-ui/core'
 import FavoriteIcon from '@material-ui/icons/Favorite'
+import SearchIcon from '@material-ui/icons/Search'
 
 import ImageList from './ImageList'
 
@@ -48,6 +56,54 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     menuButton: {
       marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block'
+      }
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto'
+      }
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    inputRoot: {
+      color: 'inherit'
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200
+        }
+      }
     },
     hide: {
       display: 'none'
@@ -115,16 +171,45 @@ export default function App () {
           <Toolbar>
             <IconButton
               color='inherit'
-              aria-label='open drawer'
               onClick={handleDrawerOpen}
               edge='start'
               className={clsx(classes.menuButton, open && classes.hide)}
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant='h6' noWrap>
+            <Typography className={classes.title} variant='h6' noWrap>
               Lemon Wallpaper
             </Typography>
+            <Tooltip title='切换数据源'>
+              <Select value={10}>
+                <MenuItem value={10}>必应壁纸（cn）</MenuItem>
+                <MenuItem value={20}>unsplash</MenuItem>
+                <MenuItem value={30}>wallhaven</MenuItem>
+              </Select>
+            </Tooltip>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder='Search'
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+            <Tooltip title='访问 Github'>
+              <IconButton>
+                <GitHub />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title='切换深色/浅色主题'>
+              <IconButton>
+                {customTheme.palette.type === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
+              </IconButton>
+            </Tooltip>
           </Toolbar>
         </AppBar>
         <Drawer
