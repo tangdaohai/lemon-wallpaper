b72dedf143f18483f9611aded57aadc39a104ec9
唐道海 <tangdaohai@outlook.com>
2020-03-10 21:58:22 +0800

    feat(dataSourceConfig): 增加数据源配置

---
+++ src/data-source-config.ts
@@ -0,0 +1,23 @@
+export default {
+  biying: {
+    key: 'biying',
+    name: '必应搜索壁纸(CN)',
+    searchUrl: 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7&nc=1582381253445&pid=hp',
+    canSearch: false,
+    showDetail: true
+  },
+  unsplash: {
+    key: 'unsplash',
+    name: 'unsplash',
+    apiUrl: '',
+    canSearch: true,
+    showDetail: false
+  },
+  wallhaven: {
+    key: 'wallhaven',
+    name: 'WallHaven',
+    apiUrl: '',
+    canSearch: true,
+    showDetail: false
+  }
+}
