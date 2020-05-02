2231a0a318597c3304d9984e9bda3d65bc765be7
唐道海 <tangdaohai@outlook.com>
2020-05-02 22:18:28 +0800

    feat(electron): controller

--- electron/controller.ts
+++ electron/controller.ts
@@ -2,29 +2,50 @@ import { EventType } from 'lemon-utils'
 import wallpaper from 'wallpaper'
 import { use } from './util/ipc-server'
 import Proxy from './service/proxy-service'
-import Download from './service/download'
+import Download from './service/download-service'
 
-function _format (isSuccess: boolean, content?: any) {
+function _success (content?: any) {
   return {
-    success: isSuccess,
+    success: true,
     content
   }
 }
+
+function _error (content?: any) {
+  return {
+    success: false,
+    content
+  }
+}
+
+function _format (isSuccess: boolean, content?: any) {
+  return isSuccess ? _success(content) : _error(content)
+}
+
 // proxy
 use(EventType.PROXY, async (ctx, data) => {
   const result = await Proxy(data.type, data.params)
-  ctx.reply(_format(true, result))
+  ctx.reply(_success(result))
 })
 
 // 下载图片
 use(EventType.DOWNLOAD, async (ctx, data) => {
-  const result = await Download(data.type, data.params)
-  ctx.reply(_format(true, result))
+  try {
+    const result = await Download(data.type, data.url)
+    ctx.reply(_success(result))
+  } catch (e) {
+    ctx.reply(_error('下载失败了'))
+  }
 })
 
 // 设置为桌面
 use(EventType.SET_DESKTOP, async (ctx, data) => {
-  const result = await Download(data.type, data.params)
-  await wallpaper.set(result.path)
-  ctx.reply(result.path === await wallpaper.get())
+  try {
+    const result = await Download(data.type, data.url)
+    await wallpaper.set(result.saveImgName)
+    ctx.reply(_format(result.saveImgName === await wallpaper.get()))
+  } catch (e) {
+    console.log(e)
+    ctx.reply(_error('设置失败'))
+  }
 })
