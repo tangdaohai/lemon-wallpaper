190ba131dcae66a715fb99fe210897639604fe1a
唐道海 <tangdaohai@outlook.com>
2020-05-13 21:44:20 +0800

    feat(show-message.tsx): fix open effect

--- src/components/show-message.tsx
+++ src/components/show-message.tsx
@@ -5,21 +5,25 @@ import MuiAlert, { Color as AlertType } from '@material-ui/lab/Alert'
 export interface ShowMessageProps {
   type?: AlertType
   content: string
-  open: boolean
+  open: boolean,
+  onClose?: (event: React.SyntheticEvent) => void
 }
 
 export default function ShowMessage (props: ShowMessageProps) {
   const [status, setStatus] = useState(false)
   useEffect(() => {
     setStatus(props.open)
-  }, [props])
+  }, [props.open])
 
   // 关闭提示框
-  const snackbarClose = (event?: React.SyntheticEvent, reason?: string) => {
+  const snackbarClose = (event: React.SyntheticEvent, reason?: string) => {
     if (reason === 'clickaway') {
       return
     }
     setStatus(false)
+    if (props.onClose) {
+      props.onClose(event)
+    }
   }
   return (
     <Snackbar
