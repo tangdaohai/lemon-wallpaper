fb08f331505d0a86ed60fc121865ea242d7def5a
唐道海 <tangdaohai@outlook.com>
2020-06-27 17:25:27 +0800

    feat(web/search): add loading

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,16 +1,31 @@
 import React, { useContext, useState, useEffect } from 'react'
+import { LinearProgress } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { EventType } from 'lemon-utils'
 import GlobalContext from '../context/global-context'
 import ipcRequest from 'electron-happy-ipc/request'
 import ImageList from './common/image-list'
 
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    // 进度条
+    linearProgress: {
+      position: 'absolute',
+      width: '100%'
+    }
+  })
+})
+
 export default function Search () {
   const { searchContent, dataSource, whParams } = useContext(GlobalContext)
+  const [loading, setLoading] = useState(false)
   const [list, setList] = useState([])
   // 页码
   const [pageNum, setPageNum] = useState(0)
   // 每页几条
   const [rowsPerPage, setRowsPerPage] = useState(12)
+
+  const classes = useStyles()
 
   useEffect(() => {
     setPageNum(0)
@@ -29,7 +44,7 @@ export default function Search () {
 
   // 图片列表发起请求
   const listRequest = async (data: any) => {
-    // setLoading(true)
+    setLoading(true)
     try {
       const result = await ipcRequest(EventType.PROXY, data)
       if (result.success) {
@@ -38,7 +53,7 @@ export default function Search () {
         // 提示
       }
     } finally {
-      // setLoading(false)
+      setLoading(false)
     }
   }
 
@@ -59,11 +74,14 @@ export default function Search () {
   }, [dataSource, searchContent, pageNum, rowsPerPage, whParams])
 
   return (
-    <ImageList
-      list={list}
-      rowsPerPage={rowsPerPage}
-      onPerPageChange={rowsPerPageChangeHandle}
-      onPageChange={pageChangeHandle}
-    />
+    <div>
+      <LinearProgress className={classes.linearProgress} style={{ display: loading ? '' : 'none' }} />
+      <ImageList
+        list={list}
+        rowsPerPage={rowsPerPage}
+        onPerPageChange={rowsPerPageChangeHandle}
+        onPageChange={pageChangeHandle}
+      />
+    </div>
   )
 }
