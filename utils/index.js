d2f97b1dfe9f8ffcab269ea300dd100680bccb5a
唐道海 <tangdaohai@outlook.com>
2020-07-04 23:57:29 +0800

    feat(utils): 调整常量配置

--- utils/index.js
+++ utils/index.js
@@ -3,7 +3,13 @@ exports.__esModule = true;
 exports.EventType = {
     PROXY: 'proxy',
     DOWNLOAD: 'download',
-    SET_DESKTOP: 'set-desktop'
+    SET_DESKTOP: 'set-desktop',
+    SELECT_DIR: 'select-dir',
+    GET_DOWNLOAD_PATH: 'get-download-path',
+    SET_DOWNLOAD_PATH: 'set-download-path',
+    GET_LOCAL_IMG: 'get-local-img',
+    SET_LOCAL_IMG_DESKTOP: 'set-local-img-desktop',
+    DELETE_LOCAL_IMG: 'delete-local-img'
 };
 exports.DataSource = {
     BING: 'bing',
