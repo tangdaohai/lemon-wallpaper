b597ee0e54faadaa488ba872d8e13665a5d72475
唐道海 <tangdaohai@outlook.com>
2020-07-09 22:10:31 +0800

    chore(web/context): 优化 ts 类型

--- src/context/global-context-provide.tsx
+++ src/context/global-context-provide.tsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
 import { DataSource } from 'lemon-utils'
-import GlobalContext from './global-context'
-import dataSourceConfig from '../data-source-config'
+import GlobalContext, { ThemeType, DataSourceType } from './global-context'
 
 interface Props {
   children: React.ReactElement | Array<React.ReactElement>
 }
 
-const cacheThemeType = (window.localStorage.getItem('themeType') || 'light') as 'light' | 'dark'
+const cacheThemeType = (window.localStorage.getItem('themeType') || 'light') as ThemeType
 
 export default function GlobalContextProvide (props: Props) {
   const [searchContent, changeSearchContent] = useState('')
-  const [themeType, setThemeType] = useState<'dark' | 'light'>(cacheThemeType)
+  const [themeType, setThemeType] = useState<ThemeType>(cacheThemeType)
   // 默认搜索必应壁纸
-  const [dataSource, changeDataSource] = useState<keyof typeof dataSourceConfig>(DataSource.BING)
+  const [dataSource, changeDataSource] = useState<DataSourceType>(DataSource.BING)
   const [whParams, changeWhParams] = useState({
     categories: [1, 1, 1],
     purity: [1, 1]
--- src/context/global-context.ts
+++ src/context/global-context.ts
@@ -1,33 +1,21 @@
 import { createContext } from 'react'
 import dataSourceConfig from '../data-source-config'
 
+export type ThemeType = 'dark' | 'light'
+export type DataSourceType = keyof typeof dataSourceConfig
 interface WhParams {
   categories: Array<number>,
   purity: Array<number>,
 }
 interface GlobalContext {
-  themeType: 'dark' | 'light',
-  setThemeType: (val: 'dark' | 'light') => void,
+  themeType: ThemeType,
+  setThemeType: (val: ThemeType) => void,
   searchContent: string,
   changeSearchContent: (val: string) => void,
-  dataSource: keyof typeof dataSourceConfig,
-  changeDataSource: (val: keyof typeof dataSourceConfig) => void,
+  dataSource: DataSourceType,
+  changeDataSource: (val: DataSourceType) => void,
   whParams: WhParams,
   changeWhParams: (params: WhParams) => void
 }
 
-const defaultContext: GlobalContext = {
-  themeType: 'light',
-  setThemeType: (val) => {},
-  searchContent: '',
-  changeSearchContent: (val: string) => {},
-  dataSource: 'unsplash',
-  changeDataSource: (val: keyof typeof dataSourceConfig) => {},
-  whParams: {
-    categories: [],
-    purity: []
-  },
-  changeWhParams: (params: WhParams) => {}
-}
-
-export default createContext(defaultContext)
+export default createContext({} as GlobalContext)
