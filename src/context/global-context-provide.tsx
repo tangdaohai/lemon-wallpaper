a086936f86d909d24a84a7fed1df4a335a764f0c
唐道海 <tangdaohai@outlook.com>
2020-07-11 22:51:05 +0800

    feat(web/context): add search type

--- src/context/global-context-provide.tsx
+++ src/context/global-context-provide.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { DataSource } from 'lemon-utils'
-import GlobalContext, { ThemeType, DataSourceType } from './global-context'
+import GlobalContext, { ThemeType, DataSourceType, Search, SearchDispatchAction } from './global-context'
 
 interface Props {
   children: React.ReactElement | Array<React.ReactElement>
@@ -8,7 +8,25 @@ interface Props {
 
 const cacheThemeType = (window.localStorage.getItem('themeType') || 'light') as ThemeType
 
+const defaultSearchValue: Search = {
+  searchContent: '',
+  dataSource: DataSource.BING,
+  whParams: {
+    categories: [1, 1, 1],
+    purity: [1, 1]
+  }
+}
+
+const reducer = (prevState: Search, action: SearchDispatchAction): Search => {
+  switch (action.type) {
+    case 'searchContent':
+      return { ...prevState, searchContent: action.value as string }
+    default:
+      return defaultSearchValue
+  }
+}
 export default function GlobalContextProvide (props: Props) {
+  const [search, searchDispatch] = useReducer(reducer, defaultSearchValue)
   const [searchContent, changeSearchContent] = useState('')
   const [themeType, setThemeType] = useState<ThemeType>(cacheThemeType)
   // 默认搜索必应壁纸
@@ -19,7 +37,7 @@ export default function GlobalContextProvide (props: Props) {
   })
 
   return (
-    <GlobalContext.Provider value={{ themeType, setThemeType, searchContent, changeSearchContent, dataSource, changeDataSource, whParams, changeWhParams }}>
+    <GlobalContext.Provider value={{ search, searchDispatch, themeType, setThemeType, searchContent, changeSearchContent, dataSource, changeDataSource, whParams, changeWhParams }}>
       {props.children}
     </GlobalContext.Provider>
   )
--- src/context/global-context.ts
+++ src/context/global-context.ts
@@ -3,13 +3,21 @@ import dataSourceConfig from '../data-source-config'
 
 export type ThemeType = 'dark' | 'light'
 export type DataSourceType = keyof typeof dataSourceConfig
+export type SearchDispatchAction = { type: keyof Search, value: string | object }
 interface WhParams {
   categories: Array<number>,
   purity: Array<number>,
 }
+export interface Search {
+  searchContent?: string,
+  dataSource: DataSourceType,
+  whParams?: WhParams
+}
 interface GlobalContext {
   themeType: ThemeType,
   setThemeType: (val: ThemeType) => void,
+  search?: Search,
+  searchDispatch?: React.Dispatch<SearchDispatchAction>,
   searchContent: string,
   changeSearchContent: (val: string) => void,
   dataSource: DataSourceType,
