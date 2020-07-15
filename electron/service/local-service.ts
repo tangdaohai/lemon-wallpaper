512a627883dbc4d18b2e50a697f3470292f7cfb8
唐道海 <tangdaohai@outlook.com>
2020-07-15 23:42:01 +0800

    feat(electron/local): 本地图片按时间从大到小排序

--- electron/service/local-service.ts
+++ electron/service/local-service.ts
@@ -7,7 +7,8 @@ interface LocalImag {
   url: string,
   downloadUrl: string,
   time: string,
-  size: string
+  size: string,
+  mtime: number
 }
 
 export async function getLocalList () {
@@ -23,11 +24,19 @@ export async function getLocalList () {
         url: 'file://' + imgPath,
         downloadUrl: imgPath,
         time: new Date(stat.mtimeMs).toLocaleString(),
-        size: stat.size / 1024 + 'KB'
+        size: stat.size / 1024 + 'KB',
+        mtime: stat.mtimeMs
       })
     }
   }
-
+  // 按时间从大到小排序（早下载的在后面）
+  result.sort((v1, v2) => {
+    if (v2.mtime - v1.mtime > 0) {
+      return 1
+    } else {
+      return -1
+    }
+  })
   return result
 }
 
