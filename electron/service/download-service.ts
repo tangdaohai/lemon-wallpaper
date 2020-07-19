49d367bbfca417391e11d1763a0d902808c9387f
唐道海 <tangdaohai@outlook.com>
2020-07-19 23:13:44 +0800

    fix(electron/download): 修复下载命名不规范问题

--- electron/service/download-service.ts
+++ electron/service/download-service.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage } from 'http'
+import { URL as UrlParse } from 'url'
 import { DataSource } from 'lemon-utils'
 import { get as getConfig } from '../config'
 import { createWriteStream, promises as fsPromises } from 'fs'
@@ -24,10 +25,19 @@ export default async function (type: string, url: string): Promise<DownLoadResul
   console.log('图片下载 url: ', url)
   // 默认图片名称
   let imgName: string = 'default-' + Date.now() + '.jpg'
-  switch (type) {
-    case DataSource.BING:
-      imgName = url.split(/^.*\//)[1]
-  }
+  try {
+    switch (type) {
+      case DataSource.BING:
+      case DataSource.WALLHAVEN:
+        imgName = url.split(/^.*\//)[1]
+        break
+      case DataSource.UNSPLASH:
+        // eslint-disable-next-line no-case-declarations
+        const urlObj = new UrlParse(url)
+        imgName = `${urlObj.pathname?.replace('/', '')}.${urlObj.searchParams.get('fm') || 'jpg'}`
+        break
+    }
+  } catch (err) {}
   const saveImgName = join(saveImgPath, imgName)
   return new Promise((resolve, reject) => {
     request(url, (err: any, response: IncomingMessage, body: any) => {
