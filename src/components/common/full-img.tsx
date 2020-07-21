d3b6ed9a5b44625e844a6adc318a25df52e24255
唐道海 <tangdaohai@outlook.com>
2020-07-21 22:57:56 +0800

    feat(web/fullImg): 优化布局

--- src/components/common/full-img.tsx
+++ src/components/common/full-img.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { Dialog, Grid, Card, CardMedia, Button } from '@material-ui/core'
+import { Dialog, Box, Card, CardMedia, CardActions, Button } from '@material-ui/core'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 const useStyles = makeStyles((theme: Theme) => {
   return createStyles({
+    card: {
+      width: '100%',
+      borderRadius: 0,
+      flexShrink: 0
+    },
     cardMedia: {
       height: 0,
       paddingTop: '56.25%',
       backgroundSize: 'contain'
-    },
-    btnEle: {
-      position: 'absolute',
-      top: '20px',
-      right: '20px'
     }
   })
 })
@@ -26,19 +26,41 @@ export default function FullImg (props: FullImgProps) {
   }, [props.url])
   return (
     <Dialog open={open} fullScreen>
-      <Grid container>
-        <Grid item sm={12}>
-          <div className={classes.btnEle}>
-            <Button onClick={() => { setOpen(false) }}>关闭</Button>
-          </div>
-          {
-            props.url &&
-              <Card>
-                <CardMedia className={classes.cardMedia} image={props.url} />
-              </Card>
-          }
-        </Grid>
-      </Grid>
+      <Box display='flex' height='100%' m={2}>
+        {/* 按钮菜单操作列 */}
+        <Box
+          width='200px'
+          mr={2}
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          flexDirection='column'
+        >
+          <Card className={classes.card} raised>
+            <CardActions>
+              <Button>下载</Button>
+              <Button>设置桌面</Button>
+            </CardActions>
+          </Card>
+          <br />
+          <Card className={classes.card} raised>
+            <CardActions>
+              <Button onClick={() => setOpen(false)}>关闭</Button>
+            </CardActions>
+          </Card>
+        </Box>
+        {/* 图片展示区域 */}
+        <Box flexGrow={1} display='flex' justifyContent='center' alignItems='center'>
+          <Box flexGrow={1}>
+            <Card className={classes.card} raised>
+              {
+                props.url &&
+                  <CardMedia className={classes.cardMedia} image={props.url} />
+              }
+            </Card>
+          </Box>
+        </Box>
+      </Box>
     </Dialog>
   )
 }
